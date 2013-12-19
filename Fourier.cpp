04338d41f01103b30116e6d8962e44b8f67f476d
wei0727 <phonneog@gmail.com>
2013-12-19 14:10:58 +0800

    FFT_1D  complex bitset

--- Fourier.cpp
+++ Fourier.cpp
@@ -1,5 +1,6 @@
 #include "Fourier.h"
 
+
 Mat centering(Mat &m){
 	Mat img ;
 	m.convertTo(img, CV_64FC1) ;
@@ -33,4 +34,55 @@ Mat zeroPadding(Mat &m, int type){
 	}
 	m.copyTo(img(cvRect(0, 0, m.cols, m.rows))) ;
 	return img ;
+}
+
+vector< vector< complex<double>>> matToVector(Mat &m){
+	vector< vector< complex<double>>> v(m.rows, vector< complex<double>>(m.cols)) ;
+	double *p ;
+	for(int r=0; r<m.rows; r++){
+		p = m.ptr<double>(r) ;
+		for(int c=0; c<m.cols; c++){
+			v[r][c] = complex<double>(p[c], 0) ;
+			//v[r][c] = 0 ;
+		}
+	}
+	return v ;
+}
+
+bitset<32> reverseBit(bitset<32> &b){
+	string bs = b.to_string() ;
+	reverse(bs.begin(), bs.end()) ;
+	return bitset<32>(bs) ;
+}
+
+vector<complex<double>> FFT_1D(vector<complex<double>> &m){
+	vector<complex<double>> v = rearrange(m) ;
+	vector<complex<double>> f = v ;
+	for(int n=2; n!=v.size(); n=n<<1){
+		vector<complex<double>> tmp(v.size()) ;
+		int nGroup = v.size()/n ;
+		int k = n >> 1 ;
+		for(int index=0; index<nGroup; index++){
+			for(int j=0; j<k; j++){
+				int u = index*n + j ;
+				double angle = CV_PI*j/k ;
+				complex<double> w(cos(angle), -sin(angle)) ;
+				tmp[u] = f[u] + f[u+k]*w ;
+				tmp[u+k] = f[u] - f[u+k]*w ;
+			}
+		}
+		f = tmp ;
+	}
+	return f ;
+}
+
+vector<complex<double>> rearrange(vector<complex<double>> &m){
+	vector<complex<double>> v(m.size()) ;
+	for(int i=0; i<m.size(); i++){
+		bitset<32> b(i) ;
+		b = reverseBit(b) ;
+		int j = b.to_ulong() ;
+		v[j] = m[i] ;
+	}
+	return v ;
 }
--- Fourier.h
+++ Fourier.h
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <bitset>
+#include <algorithm>
+#include <string>
+#include <complex>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 using namespace std;
 using namespace cv;
 
+
 Mat centering(Mat &m) ;
 Mat zeroPadding(Mat &m, int type=0) ;
+vector< vector< complex<double>>> matToVector(Mat &m) ;
+vector< complex<double>> FFT_1D(vector< complex<double>> &m) ;
+bitset<32> reverseBit(bitset<32> &b) ;
+vector< complex<double>> rearrange(vector< complex<double>> &m) ;
